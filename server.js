52d559665b2324dcb10a4376eabb0f85c451ad34
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2023-01-06 17:03:34 -0800

    installing nodemon for production

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.get("", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.post("/upload", fileupload({ createParentPath: true }), 
