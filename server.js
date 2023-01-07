6e6665b3257d32d08bc9501727cdef89e9fe666e
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2023-01-06 16:04:29 -0800

    creating a middleware with the fileupload

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const fileupload = require("express-fileupload");
 const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.get("" , (req,res) => {
-     res.sendFile(path.join(__dirname, "index.html"))
-})
+app.use(express.json());
+app.get("", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-const PORT = process.env.PORT || 5000
+app.post("/upload", fileupload({ createParentPath: true }), (req, res) => {
+    const files = req.files;
+    console.log(files);
+    return res.json({status: 'logged', message: 'logged'});
+});
 
-app.listen(PORT, () => console.log(`server is running on ${PORT}`))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
